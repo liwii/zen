10684f71c7540b396b48d9db12d7c954dbbaaa1b
Koki Ryu <kokiryu@google.com>
2022-07-19 00:07:04 +0900

    bind wl_shm

--- clients/simple-ball/src/main.cc
+++ clients/simple-ball/src/main.cc
@@ -1,16 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <wayland-client.h>
+
+struct app {
+  struct wl_display *display;
+  struct wl_registry *registry;
+  struct wl_shm *shm;
+};
+
+static void
+global_registry(void *data, struct wl_registry *registry, uint32_t id,
+    const char *interface, uint32_t version)
+{
+  struct app *app = (struct app *)data;
+  if (strcmp(interface, "wl_shm") == 0) {
+    app->shm = (struct wl_shm *)wl_registry_bind(
+        registry, id, &wl_shm_interface, version);
+  }
+}
+
+static void
+global_registry_remove(void *data, struct wl_registry *registry, uint32_t id)
+{
+  struct app *app = (struct app *)data;
+  (void)app;
+  (void)registry;
+  (void)id;
+}
+const struct wl_registry_listener registry_listener = {
+    global_registry,
+    global_registry_remove,
+};
 
 int
 main(void)
 {
-  struct wl_display *display;
-  display = wl_display_connect("zigen-0");
+  struct app app;
+  app.display = wl_display_connect("zigen-0");
 
-  if (display == nullptr) {
+  if (app.display == nullptr) {
     fprintf(stderr, "Cannot connect to the socket: zigen-0\n");
     return EXIT_FAILURE;
   }
+
+  app.registry = wl_display_get_registry(app.display);
+  if (app.registry == nullptr) {
+    fprintf(stderr, "Cannot get the registry\n");
+    return EXIT_FAILURE;
+  }
+  wl_registry_add_listener(app.registry, &registry_listener, &app);
+  wl_display_dispatch(app.display);
+  wl_display_roundtrip(app.display);
+
   return EXIT_SUCCESS;
 }
