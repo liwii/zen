21fdce6f6807f98a3ede1e2ea10d443e9fc1e1af
Koki Ryu <kokiryu@google.com>
2022-07-19 00:07:04 +0900

    Connect to the display

--- clients/simple-ball/src/main.cc
+++ clients/simple-ball/src/main.cc
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <wayland-client.h>
 
 int
 main(void)
 {
-  fprintf(stderr, "Hello, world!!\n");
+  struct wl_display *display;
+  display = wl_display_connect("zigen-0");
+
+  if (display == nullptr) {
+    fprintf(stderr, "Cannot connect to the socket: zigen-0\n");
+    return EXIT_FAILURE;
+  }
   return EXIT_SUCCESS;
 }
